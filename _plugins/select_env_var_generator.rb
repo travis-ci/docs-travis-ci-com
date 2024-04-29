1c8a3f59ef8281cc17b7e35adc337403c9960f36
Michael Mendy <montana@linux.com>
2024-04-29 10:50:18 -0700

    The functionality of the code remains the same, but these improvements enhance the code's readability, consistency, and adherence to Ruby best practices.

--- _plugins/select_env_var_generator.rb
+++ _plugins/select_env_var_generator.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
+ENVS = %w[
+  WEBHOOK_PAYLOAD_GIST_ID
+].freeze
+
 class SelectEnvVarGenerator < Jekyll::Generator
-  ENVS = %w(
-    WEBHOOK_PAYLOAD_GIST_ID
-  )
   def generate(site)
     site.config['env'] = {}
-
     ENVS.each do |env|
       site.config['env'][env] = ENV[env]
     end
